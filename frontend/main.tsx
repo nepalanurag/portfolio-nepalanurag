4426616f89174ad5041163b2c894bac1e6ea07dd
brai0 <nepalanurag72@gmail.com>
2025-08-07 16:51:36 -0700

    update

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import VercelAnalytics from "./lib/VercelAnalytics";
+import VercelAnalytics from "@/lib/VercelAnalytics";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
