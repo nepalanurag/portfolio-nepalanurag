398baffe50b2e15e31f533b460f462d20129596f
brai0 <nepalanurag72@gmail.com>
2025-08-09 03:15:41 -0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname),
     },
   },
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
+  mode: "development",
+  build: {
+    minify: false,
+  },
 });
